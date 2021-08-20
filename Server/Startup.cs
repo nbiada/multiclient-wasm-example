cdaf2cb71a3fa16aef3a923961baed904681e544
nbiada <nicola@biada.it>
2021-08-20 07:45:07 +0200

    Add project files.

---
+++ Server/Startup.cs
@@ -0,0 +1,106 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace MultiClientApplicationWasmExample.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            // FOR SINGLE APP
+            // =================================
+            //app.UseBlazorFrameworkFiles();
+            //app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //    endpoints.MapControllers();
+            //    endpoints.MapFallbackToFile("index.html");
+            //});
+
+            app.MapWhen(ctx => ctx.Request.Host.Port == 5001 ||
+                               ctx.Request.Host.Equals("firstapp.com"), first =>
+            {
+                first.Use((ctx, nxt) =>
+                {
+                    ctx.Request.Path = "/FirstApp" + ctx.Request.Path;
+                    return nxt();
+                });
+
+                first.UseBlazorFrameworkFiles("/FirstApp");
+                first.UseStaticFiles();
+                first.UseStaticFiles("/FirstApp");
+                first.UseRouting();
+
+                first.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToFile("/FirstApp/{*path:nonfile}",
+                        "FirstApp/index.html");
+                });
+            });
+
+            app.MapWhen(ctx => ctx.Request.Host.Port == 5002 ||
+                               ctx.Request.Host.Equals("secondapp.com"), second =>
+            {
+                second.Use((ctx, nxt) =>
+                {
+                    ctx.Request.Path = "/SecondApp" + ctx.Request.Path;
+                    return nxt();
+                });
+
+                second.UseBlazorFrameworkFiles("/SecondApp");
+                second.UseStaticFiles();
+                second.UseStaticFiles("/SecondApp");
+                second.UseRouting();
+
+                second.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToFile("/SecondApp/{*path:nonfile}",
+                        "SecondApp/index.html");
+                });
+            });
+        }
+    }
+}
